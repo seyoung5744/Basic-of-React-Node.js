3497fd361e0ab80fee4ba7212aa457eaeae311d5
seyoung5744 <seyoung574458@gmail.com>
2021-05-24 15:22:12 +0900

    #32 React Logout 기능 구현

--- boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -1,18 +1,40 @@
-import React, { useEffect } from 'react'
+import React, {useEffect} from 'react'
 import axios from 'axios'
 
-function LandingPage() {
+function LandingPage(porps) {
 
     useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => console.log(response.data))
+        axios
+            .get('/api/hello')
+            .then(response => console.log(response.data))
     }, [])
+
+    const onClickHandler = () => {
+        axios
+            .get('/api/users/logout')
+            .then(response => {
+                console.log(response.data)
+                if(response.data.success){
+                    porps.history.push("/login")
+                }else{
+                    alert("로그아웃 하는데 실패하였습니다.")
+                }
+            })
+    }
     return (
-        <div style = {{
-            display: 'flex', justifyContent: 'center', alignItems: 'center'
-            , width: '100%', height: '100vh'
-        }}>
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+                height: '100vh'
+            }}>
             <h2>시작 페이지</h2>
+
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
         </div>
     )
 }
